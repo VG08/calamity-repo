e452528211203876a38d1f9f9ab28844fab99ab5
VG08 <71305756+VG08@users.noreply.github.com>
2021-11-27 20:41:45 +0530

    upload initial repo

---
+++ git-push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd x86_64
+sh update_repo.sh
+
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+# Committing to the local repository with a message containing the commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin master
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
